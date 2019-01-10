6fe3025c049b9fc4483db1216fe3858daf7253dd
Bohdan Sinchuk <sinchuk140995@gmail.com>
2019-01-11 01:37:17 +0200

    add task math_expectation_and_dispersion

--- ecg_handler/tasks.py
+++ ecg_handler/tasks.py
@@ -1,7 +1,36 @@
 from __future__ import absolute_import, unicode_literals
 from celery import task
 
+from . import models
+
 
 @task()
-def task_number_one():
-    print("hello world!")
+def math_expectation_and_dispersion():
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$value",
+                "count": {"$sum": 1}
+            }
+        }
+    ]
+    count_of_values = models.Signal.objects.aggregate(*pipeline)
+    signals_count = len(models.Signal.objects)
+    probability_of_values = dict()
+    for count_of_value in count_of_values:
+        value = count_of_value['_id']
+        count = count_of_value['count']
+        probability_of_values[value] = count / signals_count
+
+    math_expecation = 0
+    for value, probability in probability_of_values.items():
+        math_expecation += value * probability
+
+    dispersion_of_values = dict()
+    for value in probability_of_values.keys():
+        dispersion_of_values[value] = pow(value - math_expecation, 2)
+
+    return {
+        'math_expecation': math_expecation,
+        'dispersion_of_values': dispersion_of_values,
+    }
--- ecg_handler/views.py
+++ ecg_handler/views.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from mongoengine.errors import ValidationError
 
 from . import models
