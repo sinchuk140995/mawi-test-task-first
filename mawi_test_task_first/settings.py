820d35e56021078a92fd4fd92a0472e58ea9426f
Bohdan Sinchuk <sinchuk140995@gmail.com>
2019-01-11 00:21:16 +0200

    celery and redis setup

--- mawi_test_task_first/settings.py
+++ mawi_test_task_first/settings.py
@@ -146,3 +146,14 @@ REST_FRAMEWORK = {
 # Connect to MongoDB
 
 connect('mawi_test_task_first')
+
+
+# Celery
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_BEAT_SCHEDULE = {}
